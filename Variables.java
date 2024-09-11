7331bd75360f984324dd7beaa4abc36c5ed030f7
rosalindc2 <rosalindc2@nycstudents.net>
2024-09-11 19:15:53 +0000

    first commit

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables
+{ 
+    public static void main (String[] args)
+    {
+        string r = "r";
+        int i = 8;
+        double n = 3.3;
+        i = i + 1;
+        boolean d = false;
+        system.out.println(r+i+n+d);
+    }
+}
+    
