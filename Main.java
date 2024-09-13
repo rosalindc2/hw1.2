7a13e74e4f59e3202335acc65b66c779607f0627
rosalindc2 <rosalindc2@nycstudents.net>
2024-09-13 13:20:40 +0000

    1

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
-public class Variables
+public class Main
 { 
     public static void main (String[] args)
     {
-        string r = "r";
+        final String r = "r";
         int i = 8;
         double n = 3.4;
         i = i + 1;
         boolean d = false;
-        system.out.println(r);
-        system.out.print(i+n);
-        system.out.print(d);
+        System.out.println(r);
+        System.out.print(i+n);
+        System.out.print(d);
     }
 }
     
